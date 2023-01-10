9d2a68728d36fd89e3291951396a80072b029af9
Dmitriy <maradonna05@yandex.ru>
2023-01-10 14:29:19 +0300

    Добавьте файлы проекта.

---
+++ LogicOperations/LogicOperations.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    int pin1, pin2;
+    printf("pin1= ");
+    scanf("%i", &pin1);
+    printf("pin2= ");
+    scanf("%i", &pin2);
+
+    if (((pin1 == 1234) && (pin2 == 4321)) || ((pin1 == 6789) && (pin2 == 9876)))
+    {
+        printf("Wellcome!");
+    }
+    else
+    {
+        printf("Incorrect pin! Try Again!");
+    }
+}
